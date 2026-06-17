df75e00cab999688147bd4e9a93a76ad8bb67ae6
Tom Korec <tom.korec@gmail.com>
2026-06-17 17:06:38 +0200

    Add PickupPoint entity and integrate Doctrine Migrations

---
+++ src/Entity/PickupPoint.php
@@ -0,0 +1,240 @@
+<?php
+
+namespace App\Entity;
+
+use App\Enum\PickupPointStatus;
+use App\Enum\PickupPointType;
+use App\Repository\PickupPointRepository;
+use DateTimeImmutable;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PickupPointRepository::class)]
+#[ORM\Table(name: 'pickup_points', options: [
+    'engine' => 'InnoDB',
+    'collation' => 'utf8mb4_czech_ci',
+    'charset' => 'utf8mb4'
+])]
+#[ORM\UniqueConstraint(
+    name: 'carrier_externalId_country',
+    columns: ['carrier', 'externalId', 'country']
+)]
+class PickupPoint
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'bigint', options: ['unsigned' => true])]
+    private ?int $id = null;
+
+    #[ORM\Column(name: 'externalId', length: 255)]
+    private ?string $externalId = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $carrier = null;
+
+    #[ORM\Column]
+    private ?PickupPointType $type = null;
+
+    #[ORM\Column]
+    private ?PickupPointStatus $status = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $city = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $address = null;
+
+    #[ORM\Column(name: 'zipCode', length: 255)]
+    private ?string $zipCode = null;
+
+    #[ORM\Column(length: 2)]
+    private ?string $country = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 10, scale: 8)]
+    private ?string $latitude = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 11, scale: 8)]
+    private ?string $longitude = null;
+
+    #[ORM\Column(name: 'openingHours', type: Types::TEXT, nullable: true)]
+    private ?string $openingHours = null;
+
+    #[ORM\Column]
+    private DateTimeImmutable $created;
+
+    public function __construct()
+    {
+        $this->created = new DateTimeImmutable();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(string $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getExternalId(): ?string
+    {
+        return $this->externalId;
+    }
+
+    public function setExternalId(string $externalId): static
+    {
+        $this->externalId = $externalId;
+
+        return $this;
+    }
+
+    public function getCarrier(): ?string
+    {
+        return $this->carrier;
+    }
+
+    public function setCarrier(string $carrier): static
+    {
+        $this->carrier = $carrier;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): static
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): static
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(string $city): static
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getAddress(): ?string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(string $address): static
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    public function getZipCode(): ?string
+    {
+        return $this->zipCode;
+    }
+
+    public function setZipCode(string $zipCode): static
+    {
+        $this->zipCode = $zipCode;
+
+        return $this;
+    }
+
+    public function getCountry(): ?string
+    {
+        return $this->country;
+    }
+
+    public function setCountry(string $country): static
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+
+    public function getLatitude(): ?string
+    {
+        return $this->latitude;
+    }
+
+    public function setLatitude(string $latitude): static
+    {
+        $this->latitude = $latitude;
+
+        return $this;
+    }
+
+    public function getLongitude(): ?string
+    {
+        return $this->longitude;
+    }
+
+    public function setLongitude(string $longitude): static
+    {
+        $this->longitude = $longitude;
+
+        return $this;
+    }
+
+    public function getOpeningHours(): ?string
+    {
+        return $this->openingHours;
+    }
+
+    public function setOpeningHours(?string $openingHours): static
+    {
+        $this->openingHours = $openingHours;
+
+        return $this;
+    }
+
+    public function getCreated(): ?DateTimeImmutable
+    {
+        return $this->created;
+    }
+
+    public function setCreated(DateTimeImmutable $created): static
+    {
+        $this->created = $created;
+
+        return $this;
+    }
+}
