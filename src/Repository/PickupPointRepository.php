385a739fbe691c28f49cd56ca444a557612e6146
Tom Korec <tom.korec@gmail.com>
2026-06-17 18:14:06 +0200

    Add methods to `PickupPointRepository` for carrier-based queries, termination, and updating pickup points

--- src/Repository/PickupPointRepository.php
+++ src/Repository/PickupPointRepository.php
@@ -3,6 +3,9 @@
 namespace App\Repository;
 
 use App\Entity\PickupPoint;
+use App\Enum\Carrier;
+use App\Enum\PickupPointStatus;
+use App\PickupPoint\Fetcher\PickupPointData;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -16,28 +19,55 @@ class PickupPointRepository extends ServiceEntityRepository
         parent::__construct($registry, PickupPoint::class);
     }
 
-    //    /**
-    //     * @return PickupPoint[] Returns an array of PickupPoint objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('p')
-    //            ->andWhere('p.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('p.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
+    public function findIdsByCarrier(Carrier $carrier): array
+    {
+        $qb = $this->createQueryBuilder('p')
+            ->select('p.id, p.externalId')
+            ->where('p.carrier = :carrier')
+            ->setParameter('carrier', $carrier);
 
-    //    public function findOneBySomeField($value): ?PickupPoint
-    //    {
-    //        return $this->createQueryBuilder('p')
-    //            ->andWhere('p.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
+        return $qb->getQuery()->getArrayResult();
+    }
+
+    /**
+     * @param array<int> $pickupPointsToTerminate
+     * @return void
+     */
+    public function terminatePickupPoints(array $pickupPointsToTerminate): void
+    {
+        if (empty($pickupPointsToTerminate)) {
+            return;
+        }
+
+        $qb = $this->createQueryBuilder('p')
+            ->update()
+            ->set('p.status', ':status')
+            ->where('p.id IN (:ids)')
+            ->setParameter('status', PickupPointStatus::TERMINATED->value)
+            ->setParameter('ids', $pickupPointsToTerminate);
+
+        $qb->getQuery()->execute();
+    }
+
+    public function updatePickupPointFromData(PickupPointData $pickupPointData): void
+    {
+        $qb = $this->createQueryBuilder('p')
+            ->update()
+            ->set('p.name', ':name')
+            ->set('p.address', ':address')
+            ->set('p.city', ':city')
+            ->set('p.postalCode', ':postalCode')
+            ->set('p.country', ':country')
+            ->where('p.id = :id')
+            ->setParameter('name', $pickupPointData->name)
+            ->setParameter('address', $pickupPointData->address)
+            ->setParameter('city', $pickupPointData->city)
+            ->setParameter('zipCode', $pickupPointData->zipCode)
+            ->setParameter('country', $pickupPointData->country)
+            ->setParameter('id', $pickupPointData->id);
+
+        $qb->getQuery()->execute();
+    }
+
+
 }
