9cbbbb3c5e7fa4f0031b98169075cb12661de33a
Tom Korec <tom.korec@gmail.com>
2026-06-22 08:08:42 +0200

    Extend `PickupPointFetcher` with `carrier` method and optional `FetchConfig` parameter; add `FETCHER_TAG` constant with autoconfiguration

--- src/PickupPoint/Fetcher/PickupPointFetcher.php
+++ src/PickupPoint/Fetcher/PickupPointFetcher.php
@@ -2,10 +2,20 @@
 
 namespace App\PickupPoint\Fetcher;
 
+use App\Enum\Carrier;
+use Symfony\Component\DependencyInjection\Attribute\AutoconfigureTag;
+
+
+#[AutoconfigureTag(PickupPointFetcher::FETCHER_TAG)]
 interface PickupPointFetcher
 {
+    public const string FETCHER_TAG = 'app.pickup_point_fetcher';
+
     /**
      * @return iterable<PickupPointData>
      */
-    public function fetch(): iterable;
+    public function fetch(?FetchConfig $config = null): iterable;
+
+    public function carrier(): Carrier;
+
 }
