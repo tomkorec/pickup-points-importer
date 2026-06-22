4e3a3cc0fd16e5d7f64c5eb8492a07f86b78fa7f
Tom Korec <tom.korec@gmail.com>
2026-06-22 08:08:28 +0200

    Make `FetcherLocator` a `readonly` class and implement carrier-specific fetcher resolution logic with `AutowireIterator`.

--- src/PickupPoint/Fetcher/FetcherLocator.php
+++ src/PickupPoint/Fetcher/FetcherLocator.php
@@ -4,15 +4,29 @@ namespace App\PickupPoint\Fetcher;
 
 use App\Enum\Carrier;
 use App\PickupPoint\Exception\FetcherNotFound;
+use Symfony\Component\DependencyInjection\Attribute\AutowireIterator;
 
-class FetcherLocator
+final readonly class FetcherLocator
 {
+
+    public function __construct(
+        #[AutowireIterator(tag: PickupPointFetcher::FETCHER_TAG)]
+        private iterable $fetchers,
+    ) {}
+
     /**
      * @throws FetcherNotFound
      */
     public function locate(Carrier $carrier): PickupPointFetcher
     {
-        throw new FetcherNotFound();
+        foreach ($this->fetchers as $fetcher) {
+
+            if ($fetcher->carrier() === $carrier) {
+                return $fetcher;
+            }
+        }
+
+        throw new FetcherNotFound(sprintf('No fetcher found for carrier %s', $carrier->value));
     }
 
 }
